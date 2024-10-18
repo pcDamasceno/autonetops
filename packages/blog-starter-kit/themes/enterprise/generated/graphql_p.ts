438e06707d34398af034f82cfe52421e2aae4803
pdamasceno <pedro.damasceno@hotmail.com>
2024-10-18 22:01:34 +0200

    add to seriesbyPub4

--- packages/blog-starter-kit/themes/enterprise/generated/graphql_p.ts
+++ packages/blog-starter-kit/themes/enterprise/generated/graphql_p.ts
@@ -3,10 +3,18 @@ import  {
   Badge,  
   Scalars,
   InputMaybe,
+  Content,
   Exact,
+  Maybe,
+  UserConnection,
+  UserPostConnection,
+  UserPublicationsConnection,
   PublicationNavigationType,
+  SocialMediaLinks,
+  Tag,
   TagPostsByPublicationQuery,
-  TagPostsByPublicationQueryVariables
+  TagPostsByPublicationQueryVariables,
+  UserTagsConnection,
 
 } from './graphql'
 
@@ -42,13 +50,69 @@ export type SeriesByPublicationQuery = {
           name: string;
           slug: string;
           author: {
-            __typename?: 'User'; // Adjust according to your schema
-            id: string;
-            name: string;
+            __typename?: 'User';
+            /**
+             * Whether or not the user is an ambassador.
+             * @deprecated Ambassadors program no longer active. Will be removed after 02/01/2024
+             */
             ambassador: Scalars['Boolean']['output'];
+            /** The availability of the user based on tech stack and interests. Shown on the "I am available for" section in user's profile. */
+            availableFor?: Maybe<Scalars['String']['output']>;
+            /** Returns a list of badges that the user has earned. Shown on blogs /badges page. Example - https://iamshadmirza.com/badges */
             badges: Array<Badge>;
-            deactivatedAt?: boolean | null;
-            followersCount: number;
+            /** The bio of the user. Visible in about me section of the user's profile. */
+            bio?: Maybe<Content>;
+            /**
+             * The bio of the user. Visible in about me section of the user's profile.
+             * @deprecated Will be removed on 26/10/2023. Use bio instead of bioV2
+             */
+            bioV2?: Maybe<Content>;
+            /** The date the user joined Hashnode. */
+            dateJoined?: Maybe<Scalars['DateTime']['output']>;
+            /** Whether or not the user is deactivated. */
+            deactivated: Scalars['Boolean']['output'];
+            /** The users who are following this user */
+            followers: UserConnection;
+            /** The number of users that follow the requested user. Visible in the user's profile. */
+            followersCount: Scalars['Int']['output'];
+            /**
+             * Whether or not the authenticated user follows this user.
+             * Returns false if the authenticated user this user.
+             */
+            following: Scalars['Boolean']['output'];
+            /** The number of users that this user is following. Visible in the user's profile. */
+            followingsCount: Scalars['Int']['output'];
+            /** The users which this user is following */
+            follows: UserConnection;
+            /**
+             * Whether or not this user follows the authenticated user.
+             * Returns false if the authenticated user this user.
+             */
+            followsBack: Scalars['Boolean']['output'];
+            /** The ID of the user. It can be used to identify the user. */
+            id: Scalars['ID']['output'];
+            /** Whether or not this is a pro user. */
+            isPro: Scalars['Boolean']['output'];
+            /** The location of the user. */
+            location?: Maybe<Scalars['String']['output']>;
+            /** The name of the user. */
+            name: Scalars['String']['output'];
+            /** Returns the list of posts the user has published. */
+            posts: UserPostConnection;
+            /** The URL to the profile picture of the user. */
+            profilePicture?: Maybe<Scalars['String']['output']>;
+            /** Publications associated with the user. Includes personal and team publications. */
+            publications: UserPublicationsConnection;
+            /** The social media links of the user. Shown on the user's profile. */
+            socialMediaLinks?: Maybe<SocialMediaLinks>;
+            /** The tagline of the user. Shown on the user's profile below the name. */
+            tagline?: Maybe<Scalars['String']['output']>;
+            /** Returns a list of tags that the user follows. */
+            tagsFollowing: Array<Tag>;
+            /** Returns list of tags from user's expertise. Shown on the user's profile. */
+            techStack: UserTagsConnection;
+            /** The username of the user. It is unique and tied with user's profile URL. Example - https://hashnode.com/@username */
+            username: Scalars['String']['output'];
           }
           createdAt: string;
           sortOrder: number;
