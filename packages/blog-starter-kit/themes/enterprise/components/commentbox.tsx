8e67824467bbad7b54d46210ee5fcab4298da417
pdamasceno <pedro.damasceno@hotmail.com>
2024-10-20 18:07:46 +0200

    scape commentbox

--- packages/blog-starter-kit/themes/enterprise/components/commentbox.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/commentbox.tsx
@@ -3,7 +3,7 @@ export const CommentBox = () => {
         <div id="test">
         <div className="commentbox"></div>
         <script src="https://unpkg.com/commentbox.io/dist/commentBox.min.js"></script>
-        <script>commentBox('5717605914509312-proj')</script>
+        <script>commentBox("5717605914509312-proj")</script>
         </div>
     )
 }
