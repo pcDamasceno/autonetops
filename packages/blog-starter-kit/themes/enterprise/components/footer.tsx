e918fcff584d803fa56d839f35a02aaaacc8290a
pdamasceno <pedro.damasceno@hotmail.com>
2024-10-21 17:13:44 +0200

    footer adjustment

--- packages/blog-starter-kit/themes/enterprise/components/footer.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/footer.tsx
@@ -113,7 +113,7 @@ export const Footer = () => {
 							<p className="mb-2 font-semibold text-slate-600 dark:text-neutral-200">Other links</p>
 							<ul className="flex flex-col gap-1 text-slate-700 dark:text-neutral-300">
 								<li>
-									<Link href="about-me" className="hover:underline">
+									<Link href="/about-me" className="hover:underline">
 										About
 									</Link>
 								</li>
