48f2131f3a2e99d51201fc7f2deb5cd4ee814473
pdamasceno_ <pedro.damasceno19@gmail.com>
2024-10-23 14:27:23 +0200

    Update commentbox.tsx
    Use utterances for commenting

--- packages/blog-starter-kit/themes/enterprise/components/commentbox.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/commentbox.tsx
@@ -1,1 +1,23 @@
-// Remarkbox still checking
+import { useEffect } from 'react';
+
+const Comments = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://utteranc.es/client.js';
+    script.async = true;
+    script.crossOrigin = 'anonymous';
+    script.setAttribute('repo', 'pcDamasceno/autonetops-comments');
+    script.setAttribute('issue-term', 'pathname');
+    script.setAttribute('label', 'Ut');
+    script.setAttribute('theme', 'github-light');
+    
+    const commentsDiv = document.getElementById('comments');
+    if (commentsDiv) {
+      commentsDiv.appendChild(script);
+    }
+  }, []);
+
+  return <div id="comments"></div>;
+};
+
+export default Comments;
